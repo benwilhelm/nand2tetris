d194a86a7b139fee2acb7b3de7a9a09b750029ea
Ben Wilhelm <ben@doublebeamdesign.com>
2017-12-26 15:31:57 -0700

    Machine Language exercise: multiply

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,33 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+  @i
+  M=0
+  @R2
+  M=0
+
+(LOOP)
+
+  @i
+  D=M
+  @R1
+  D=D-M  // D = iterations - factor2
+  @END
+  D;JGE  // if D >= 0, goto END
+
+  @R2
+  D=M
+  @R0
+  D=D+M
+  @R2
+  M=D
+
+  @i     // add 1 to iterator
+  M=M+1
+  @LOOP
+  0;JMP
+
+(END)
+  @END
+  0;JMP
